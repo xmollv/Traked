ea5de14dbd86b9357c55dd0123604da567c9b478
Xavi Moll <xmollv@gmail.com>
2016-01-13 21:12:23 +0100

    Created the base of the project

---
+++ ProyectoDAM/BasicCell.swift
@@ -0,0 +1,9 @@
+//
+//  BasicCell.swift
+//  ProyectoDAM
+//
+//  Created by Xavi Moll Villalonga on 13/01/16.
+//  Copyright © 2016 TaniaXavi. All rights reserved.
+//
+
+import Foundation
---
+++ ProyectoDAM/ThirdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.swift
+//  ProyectoDAM
+//
+//  Created by Xavi Moll Villalonga on 13/01/16.
+//  Copyright © 2016 TaniaXavi. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
