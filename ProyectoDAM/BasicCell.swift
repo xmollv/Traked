4a52156389b36eb731d6ea4ade207b1861b51ee6
Xavi Moll <xmollv@gmail.com>
2016-01-13 21:18:37 +0100

    Displaying test data on the collection view to check if it works

--- ProyectoDAM/BasicCell.swift
+++ ProyectoDAM/BasicCell.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2016 TaniaXavi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class BasicCell{
+    
+}
--- ProyectoDAM/FirstViewController.swift
+++ ProyectoDAM/FirstViewController.swift
@@ -9,10 +9,66 @@
 import UIKit
 
 class FirstViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collectionView.delegate = self;
+        self.collectionView.dataSource = self;
     }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return arrayOfBadgesGlobal.count
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCell", forIndexPath: indexPath)
+        let img = cell.viewWithTag(1) as! UIImageView
+        img.image = UIImage(named: "first")
+        //img.image = arrayOfBadgesGlobal[indexPath.row].imageStored!
+        
+        /*if let image = arrayOfBadgesGlobal[indexPath.row].imageStored {
+            img.image = image
+        }*/
+        
+        let text = cell.viewWithTag(2) as! UILabel
+        text.text = "Test"
+        //text.text = arrayOfBadgesGlobal[indexPath.row].name!
+        return cell
+    }
+    
+    /*func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let vistaDetalle: DetailBadgeViewController = self.storyboard?.instantiateViewControllerWithIdentifier("DetailBadgeViewController") as! DetailBadgeViewController
+        //vistaDetalle.imagen = arrayOfBadgesGlobal[indexPath.row].imageStored!
+        vistaDetalle.imagenString = arrayOfBadgesGlobal[indexPath.row].image!
+        vistaDetalle.titleBadge = arrayOfBadgesGlobal[indexPath.row].name!;
+        vistaDetalle.descriptionBadge = arrayOfBadgesGlobal[indexPath.row].description!;
+        if let winDateRAW = arrayOfBadgesGlobal[indexPath.row].winDate {
+            
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZ"
+            let winDate = dateFormatter.dateFromString(winDateRAW)
+            
+            
+            let myCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+            
+            let myComponents = myCalendar.components(NSCalendarUnit.Day, fromDate: winDate!)
+            let day = myComponents.day
+            let myComponents2 = myCalendar.components(NSCalendarUnit.Month, fromDate: winDate!)
+            let month = myComponents2.month
+            let myComponents3 = myCalendar.components(NSCalendarUnit.Year, fromDate: winDate!)
+            let year = myComponents3.year
+            
+            vistaDetalle.date = "Desbloqueaste esta insignia el \(day)-\(month)-\(year)"
+        } else {
+            vistaDetalle.date = ""
+        }
+        self.presentViewController(vistaDetalle, animated: true, completion: nil)
+    }*/
     
     
 }
