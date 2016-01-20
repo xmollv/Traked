9ae47f7f9f7949fc72ca9fe9aefab1376571c6fd
Xavi Moll <xmollv@gmail.com>
2016-01-20 16:46:06 +0100

    Added client id key

---
+++ ProyectoDAM/TraktKeys.swift
@@ -0,0 +1,14 @@
+//
+//  TraktKeys.swift
+//  ProyectoDAM
+//
+//  Created by Xavi Moll Villalonga on 19/01/16.
+//  Copyright © 2016 TaniaXavi. All rights reserved.
+//
+
+import Foundation
+
+
+class TraktKeys {
+    let clientId = "bd184c0b9d554dd55a34e8b73e0a294524a8b66d4ae56da7261558c846645bf5"
+}
