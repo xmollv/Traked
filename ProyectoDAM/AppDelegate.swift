6bd4f169817dfbf2d0a980c478dc1b9f9c78ecca
Xavi Moll <xmollv@gmail.com>
2016-01-21 23:28:55 +0100

    Getting the code returned if the user grants access to our app. Now we need to exchange the code for the user token!

--- ProyectoDAM/AppDelegate.swift
+++ ProyectoDAM/AppDelegate.swift
@@ -40,6 +40,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        print(url.query)
+        return true
+    }
 
 
 }
