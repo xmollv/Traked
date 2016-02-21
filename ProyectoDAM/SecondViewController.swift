13cb4833dec6b78a94af7af76651d8ff6bcdeeb5
Xavi Moll <xmollv@gmail.com>
2016-02-21 17:12:50 +0100

    Major refactoring started. We will reduce the main app to 3 VC (Watchlist - Watching - Watched).

--- ProyectoDAM/SecondViewController.swift
+++ ProyectoDAM/SecondViewController.swift
@@ -12,9 +12,7 @@ import AlamofireImage
 
 class SecondViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var collectionView: UICollectionView!
-    
     
     var arrayOfTvShows = [ShowOrMovie]()
     var arrayOfMovies = [ShowOrMovie]()
@@ -39,104 +37,42 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
                 print("Request failed with error: \(error)")
             }
         }
-        
-        Alamofire.request(.GET, "https://api-v2launch.trakt.tv/movies/popular?extended=images&page=1&limit=99", headers: Helper().getApiHeaders()).responseJSON{ response in
-            switch response.result {
-            case .Success (let JSON):
-                if let movies = JSON as? [[String:AnyObject]] {
-                    for movie in movies{
-                        self.arrayOfMovies.append(ShowOrMovie(dictionary: movie)!)
-                    }
-                    self.collectionView.reloadData()
-                    hudView.removeFromSuperview()
-                }
-            case .Failure (let error):
-                self.showSimpleAlert("¡Error!", message: "Ha habido un problema al realizar la petición al servidor. Vuelve a intentarlo en unos minutos.", buttonText: "Volver a intentarlo.")
-                print("Request failed with error: \(error)")
-            }
-        }
     }
     
     override func viewDidAppear(animated: Bool) {
         self.collectionView.resetScrollPositionToTop()
     }
     
-    
-    @IBAction func segmentedControlAction(sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0 {
-            collectionView.reloadData()
-            self.collectionView.resetScrollPositionToTop()
-        } else if sender.selectedSegmentIndex == 1 {
-            collectionView.reloadData()
-            self.collectionView.resetScrollPositionToTop()
-        }
-    }
-    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if segmentedControl.selectedSegmentIndex == 0 {
             return arrayOfTvShows.count
-        } else {
-            return arrayOfMovies.count
-        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCellDiscover", forIndexPath: indexPath) as! BasicCellDiscover
         
         cell.imageView.image = UIImage(named: "Grey background")
-        
-        if segmentedControl.selectedSegmentIndex == 0 {
             
-            if let thumb = arrayOfTvShows[indexPath.row].images!.poster!.thumb {
-                cell.imageView.af_setImageWithURL(NSURL(string: thumb)!)
-            } else if let medium = arrayOfTvShows[indexPath.row].images!.poster!.medium {
-                cell.imageView.af_setImageWithURL(NSURL(string: medium)!)
-            } else if let full = arrayOfTvShows[indexPath.row].images!.poster!.full {
-                cell.imageView.af_setImageWithURL(NSURL(string: full)!)
-            } else {
-                cell.imageView.image = UIImage(named: "No image")
-            }
-            
+        if let thumb = arrayOfTvShows[indexPath.row].images!.poster!.thumb {
+            cell.imageView.af_setImageWithURL(NSURL(string: thumb)!)
+        } else if let medium = arrayOfTvShows[indexPath.row].images!.poster!.medium {
+            cell.imageView.af_setImageWithURL(NSURL(string: medium)!)
+        } else if let full = arrayOfTvShows[indexPath.row].images!.poster!.full {
+            cell.imageView.af_setImageWithURL(NSURL(string: full)!)
         } else {
-            
-            if let thumb = arrayOfMovies[indexPath.row].images!.poster!.thumb {
-                cell.imageView.af_setImageWithURL(NSURL(string: thumb)!)
-            } else if let medium = arrayOfMovies[indexPath.row].images!.poster!.medium {
-                cell.imageView.af_setImageWithURL(NSURL(string: medium)!)
-            } else if let full = arrayOfMovies[indexPath.row].images!.poster!.full {
-                cell.imageView.af_setImageWithURL(NSURL(string: full)!)
-            } else {
-                cell.imageView.image = UIImage(named: "No image")
-            }
+            cell.imageView.image = UIImage(named: "No image")
         }
-        
+
         return cell
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        if segmentedControl.selectedSegmentIndex == 0 {
-            performSegueWithIdentifier("ShowEpisodeListFromDiscover", sender: nil)
-        } else {
-            performSegueWithIdentifier("ShowMovieDetailsFromDiscover", sender: nil)
-        }
+        performSegueWithIdentifier("ShowEpisodeListFromDiscover", sender: nil)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "ShowEpisodeListFromDiscover" {
-            let vc = segue.destinationViewController as! TableShowsViewController
-            let indexPath = collectionView.indexPathsForSelectedItems()
-            vc.tvShow = arrayOfTvShows[indexPath![0].row]
-            //vc.showId = arrayOfTvShows[indexPath![0].row].ids!.trakt!
-            //vc.showTitle = arrayOfTvShows[indexPath![0].row].title!
-            
-        } else if segue.identifier == "ShowMovieDetailsFromDiscover"{
-            let vc = segue.destinationViewController as! MovieEpisodeDetailsViewController
-            let indexPath = collectionView.indexPathsForSelectedItems()
-            vc.movie = arrayOfMovies[indexPath![0].row]
-            //vc.elementId = arrayOfMovies[indexPath![0].row].ids!.trakt!
-            //vc.elementTitle = arrayOfMovies[indexPath![0].row].title!
-        }
-        
+        let vc = segue.destinationViewController as! TableShowsViewController
+        let indexPath = collectionView.indexPathsForSelectedItems()
+        vc.tvShow = arrayOfTvShows[indexPath![0].row]
     }
     
     func showSimpleAlert(title: String, message: String, buttonText: String){
