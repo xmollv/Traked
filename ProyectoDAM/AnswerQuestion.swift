3f5c1882403e3262ab63487f2c6b1b7fa04210bb
Tania Fontcuberta Mercadal <taniafontcubertamercadal@gmail.com>
2016-02-27 20:09:24 +0100

    Creado modelo para preguntas y respuestas de la tabla FAQ.

---
+++ ProyectoDAM/AnswerQuestion.swift
@@ -0,0 +1,21 @@
+//
+//  AnswerQuestion.swift
+//  ProyectoDAM
+//
+//  Created by Tania Fontcuberta Mercadal on 27/2/16.
+//  Copyright © 2016 TaniaXavi. All rights reserved.
+//
+
+
+import Foundation
+
+class AnswerQuestion {
+    var question : String
+    var answer : String
+    
+    init(question: String, answer : String){
+        self.question = question
+        self.answer = answer
+        
+    }
+}
